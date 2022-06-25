1e31c2483d0701d7c0a26386ab6555fee2d96761
Sinan Tok <sinantok@outlook.com>
2022-06-25 23:33:34 +0300

    consumer operations

--- ConsumerConsole/Program.cs
+++ ConsumerConsole/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CoreLibrary;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+// See https://aka.ms/new-console-template for more information
+
+
+
 Console.WriteLine("Hello, World!");
+
+
+ConsumeSub<ContentCreatedEvent>(Constants.ContentTopic, Constants.ContentCreatedSubName, i =>
+{
+    Console.WriteLine($"ContentCreatedEvent ReceivedMessage with id: {i.Id}, ContentType: {i.ContentType}");
+}).Wait();
+
+
+ConsumeSub<ContentDeletedEvent>(Constants.ContentTopic, Constants.ContentDeletedSubName, i =>
+{
+    Console.WriteLine($"ContentDeletedEvent ReceivedMessage with id: {i.Id}");
+}).Wait();
+
+Console.WriteLine("\n\n\n");
+
+Console.ReadLine();
+
+
+
+static async Task ConsumeSub<T>(string topicName, string subName, Action<T> receivedAction)
+{
+    ISubscriptionClient client = new SubscriptionClient(Constants.ConnectionString, topicName, subName);
+
+    client.RegisterMessageHandler(async (message, ct) =>
+    {
+        var model = JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(message.Body));
+
+        receivedAction(model);
+
+        await Task.CompletedTask;
+    },
+    new MessageHandlerOptions(i => Task.CompletedTask));
+    
+    Console.WriteLine($"{typeof(T).Name} is listening....");
+}
